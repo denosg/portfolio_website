feb00e7c6a61a1a0ab99eb9f4c0129511bd421b1
denos <costelasdenis@gmail.com>
2023-05-21 14:03:50 +0300

    added photo + column text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import './homepage.dart';
 
@@ -13,6 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.montserratTextTheme(),
+      ),
       home: HomePage(),
     );
   }
