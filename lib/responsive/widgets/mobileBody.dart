0008af6a86f07b3d06ec977edfc864c4f3cfd4de
denos <costelasdenis@gmail.com>
2023-05-21 13:06:53 +0300

    Added loading.gif for when website loads

--- lib/responsive/widgets/mobileBody.dart
+++ lib/responsive/widgets/mobileBody.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class MobileBody extends StatelessWidget {
-  const MobileBody({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
