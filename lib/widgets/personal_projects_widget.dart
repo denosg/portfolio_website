64cb6cd1dfec8f6aefedb8fc1506f3677fac4bcb
denos <costelasdenis@gmail.com>
2023-05-23 17:43:43 +0300

    god help carousel slider

--- lib/widgets/personal_projects_widget.dart
+++ lib/widgets/personal_projects_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
 import 'package:portfolio_website/providers/photo_list.dart';
 import 'package:portfolio_website/widgets/project_widget.dart';
 
@@ -7,43 +9,86 @@ class PersonalProjects extends StatefulWidget {
   State<PersonalProjects> createState() => _PersonalProjectsState();
 }
 
-class _PersonalProjectsState extends State<PersonalProjects>
-    with SingleTickerProviderStateMixin {
+class _PersonalProjectsState extends State<PersonalProjects> {
+  final controller = CarouselController();
+
+  int visibleStartIndex = 0; // Starting index of visible photos
+  int visibleEndIndex = 3; // Ending index of visible photos
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(22),
-      child: Stack(children: [
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
+      child: Row(
+        children: [
+          CarouselSlider.builder(
+            itemCount: projectPhotoLists.length,
+            options: CarouselOptions(
+              enableInfiniteScroll: false,
+              viewportFraction: 1.0,
+              onPageChanged: (index, _) {
+                setState(() {
+                  visibleStartIndex = index;
+                  visibleEndIndex = index + 3;
+                });
+              },
+            ),
+            itemBuilder: (context, index, _) {
+              return Opacity(
+                opacity:
+                    (index >= visibleStartIndex && index <= visibleEndIndex)
+                        ? 1.0
+                        : 0.0,
+                child: ProjectWidget(photoList: projectPhotoLists[index]),
+              );
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ProjectWidget(photoList: calcSumList),
-              ProjectWidget(photoList: questRealmList),
-              ProjectWidget(photoList: fakeLocList),
-              ProjectWidget(photoList: beyondFirstList),
+              IconButton(
+                icon: const Icon(Icons.arrow_left),
+                onPressed: () {
+                  if (visibleStartIndex > 0) {
+                    setState(() {
+                      visibleStartIndex--;
+                      visibleEndIndex--;
+                    });
+                  }
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.arrow_right),
+                onPressed: () {
+                  if (visibleEndIndex < projectPhotoLists.length - 1) {
+                    setState(() {
+                      visibleStartIndex++;
+                      visibleEndIndex++;
+                    });
+                  }
+                },
+              ),
             ],
           ),
-        ),
-        Positioned(
-          top: 0,
-          bottom: 0,
-          left: 0,
-          child: IconButton(
-            icon: const Icon(Icons.arrow_left),
-            onPressed: null,
-          ),
-        ),
-        Positioned(
-          top: 0,
-          bottom: 0,
-          right: 0,
-          child: IconButton(
-            icon: const Icon(Icons.arrow_right),
-            onPressed: null,
-          ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
+
+  List<List<String>> projectPhotoLists = [
+    calcSumList,
+    questRealmList,
+    fakeLocList,
+    beyondFirstList,
+  ];
 }
