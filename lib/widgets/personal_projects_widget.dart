73391ecea6f2783f012f4387b659347e9cfe5428
denos <costelasdenis@gmail.com>
2023-05-23 21:03:34 +0300

    done carousel

--- lib/widgets/personal_projects_widget.dart
+++ lib/widgets/personal_projects_widget.dart
@@ -6,6 +6,8 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:portfolio_website/providers/photo_list.dart';
 import 'package:portfolio_website/widgets/project_widget.dart';
 
+// TODO: add description to apps
+
 class PersonalProjects extends StatefulWidget {
   @override
   State<PersonalProjects> createState() => _PersonalProjectsState();
@@ -13,34 +15,61 @@ class PersonalProjects extends StatefulWidget {
 
 class _PersonalProjectsState extends State<PersonalProjects> {
   final controller = CarouselController();
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider.builder(
-      options: CarouselOptions(
-        aspectRatio: 2.0,
-        enlargeCenterPage: false,
-        viewportFraction: 1,
-        autoPlay: true,
-        autoPlayAnimationDuration: Duration(seconds: 1),
-        pauseAutoPlayOnTouch: true,
-      ),
-      itemCount: (projectPhotoLists.length / 2).round(),
-      itemBuilder: (context, index, realIdx) {
-        final int first = index * 2;
-        final int second = first + 1;
-        return Row(
-          children: [first, second].map((idx) {
-            return Expanded(
-              flex: 1,
-              child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 10),
-                child: ProjectWidget(photoList: projectPhotoLists[idx]),
-              ),
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        CarouselSlider.builder(
+          carouselController: controller,
+          options: CarouselOptions(
+            aspectRatio: 2.0,
+            enlargeCenterPage: false,
+            viewportFraction: 1,
+            onPageChanged: (index, reason) {
+              setState(() {
+                currentPageIndex = index;
+              });
+            },
+          ),
+          itemCount: (projectPhotoLists.length / 2).round(),
+          itemBuilder: (context, index, realIdx) {
+            final int first = index * 2;
+            final int second = first + 1;
+            return Row(
+              children: [first, second].map((idx) {
+                return Expanded(
+                  flex: 1,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: ProjectWidget(photoList: projectPhotoLists[idx]),
+                  ),
+                );
+              }).toList(),
             );
-          }).toList(),
-        );
-      },
+          },
+        ),
+        Positioned(
+          left: 16,
+          child: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              controller.previousPage();
+            },
+          ),
+        ),
+        Positioned(
+          right: 16,
+          child: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {
+              controller.nextPage();
+            },
+          ),
+        ),
+      ],
     );
   }
 
