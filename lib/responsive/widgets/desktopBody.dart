90f6e8903d9908a579cad09262a16d69b0b86518
denos <costelasdenis@gmail.com>
2023-05-24 19:53:33 +0300

    on the good path (momentarily)

--- lib/responsive/widgets/desktopBody.dart
+++ lib/responsive/widgets/desktopBody.dart
@@ -7,18 +7,8 @@ import 'package:portfolio_website/widgets/personal_projects_widget.dart';
 import '/widgets/info_widget.dart';
 
 class DesktopBody extends StatelessWidget {
-  ScrollController _scrollController = ScrollController();
-
-  void scrolltoHome() {
-    _scrollController.animateTo(
-      0,
-      duration: const Duration(milliseconds: 500),
-      curve: Curves.easeInOut,
-    );
-  }
-
   void downloadResume() {
-    const resumePath = 'assets/resume.pdf'; // Path to your resume file
+    const resumePath = 'assets/resume.pdf';
     final anchorElement = html.AnchorElement(href: resumePath);
     anchorElement.download = 'resume.pdf';
     anchorElement.click();
@@ -29,9 +19,9 @@ class DesktopBody extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          const AppButton(scrollTo: null, text: 'Home'),
-          const AppButton(scrollTo: null, text: 'About'),
-          const AppButton(scrollTo: null, text: 'Projects'),
+          AppButton(scrollTo: null, text: 'Home'),
+          AppButton(scrollTo: null, text: 'About'),
+          AppButton(scrollTo: null, text: 'Projects'),
           TextButton(
             onPressed: downloadResume,
             child: const Text('Resume',
@@ -42,71 +32,64 @@ class DesktopBody extends StatelessWidget {
           ),
         ],
       ),
-      body: Scrollable(
-          controller: _scrollController,
-          viewportBuilder: (context, position) {
-            return SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        // info column
-                        InfoWidget(),
-                        // Image with me
-                        SizedBox(
-                          width: 200,
-                          height: 200,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(50),
-                            child: Image.asset(
-                              'pozaCuMnLow.jpg',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 35),
-                    const Center(
-                      child: FadingTextAnimation(
-                          text:
-                              "Anything is easy if you do what has to be done."),
-                    ),
-                    const SizedBox(height: 35),
-                    // about me section
-                    const Padding(
-                      padding: EdgeInsets.only(left: 50),
-                      child: Text(
-                        'About me',
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // info column
+                  InfoWidget(),
+                  // Image with me
+                  SizedBox(
+                    width: 200,
+                    height: 200,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.asset(
+                        'pozaCuMnLow.jpg',
                       ),
                     ),
-                    const SizedBox(height: 15),
-                    AboutWidget(),
-                    const SizedBox(height: 35),
-                    // personal projects section
-                    const Padding(
-                      padding: EdgeInsets.only(left: 50),
-                      child: Text(
-                        'Personal projects',
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 300,
-                      child: PersonalProjects(),
-                    ),
-                  ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 35),
+              const Center(
+                child: FadingTextAnimation(
+                    text: "Anything is easy if you do what has to be done."),
+              ),
+              const SizedBox(height: 35),
+              // about me section
+              const Padding(
+                padding: EdgeInsets.only(left: 50),
+                child: Text(
+                  'About me',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                 ),
               ),
-            );
-          }),
+              const SizedBox(height: 15),
+              AboutWidget(),
+              const SizedBox(height: 35),
+              // personal projects section
+              const Padding(
+                padding: EdgeInsets.only(left: 50),
+                child: Text(
+                  'Personal projects',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 300,
+                child: PersonalProjects(),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
