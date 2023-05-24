07a1edef8e4d982bc657dbb754ebd7a73cbf231d
denos <costelasdenis@gmail.com>
2023-05-24 19:42:39 +0300

    mistakes have been made

--- lib/app_bar_section/app_button.dart
+++ lib/app_bar_section/app_button.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppButton extends StatelessWidget {
-  final VoidCallback? scrollTo;
+  final Function? scrollTo;
   final String text;
 
-  const AppButton({required this.scrollTo, required this.text});
+  AppButton({required this.scrollTo, required this.text});
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: scrollTo,
+      onPressed: () => scrollTo,
       child: Text(text,
           style: const TextStyle(fontWeight: FontWeight.w900, fontSize: 20)),
     );
--- lib/responsive/widgets/desktopBody.dart
+++ lib/responsive/widgets/desktopBody.dart
@@ -7,6 +7,16 @@ import 'package:portfolio_website/widgets/personal_projects_widget.dart';
 import '/widgets/info_widget.dart';
 
 class DesktopBody extends StatelessWidget {
+  ScrollController _scrollController = ScrollController();
+
+  void scrolltoHome() {
+    _scrollController.animateTo(
+      0,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
   void downloadResume() {
     const resumePath = 'assets/resume.pdf'; // Path to your resume file
     final anchorElement = html.AnchorElement(href: resumePath);
@@ -19,7 +29,7 @@ class DesktopBody extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          const AppButton(scrollTo: null, text: 'Home'),
+          const AppButton(scrollTo: scrolltoHome, text: 'Home'),
           const AppButton(scrollTo: null, text: 'About'),
           const AppButton(scrollTo: null, text: 'Projects'),
           TextButton(
@@ -32,64 +42,71 @@ class DesktopBody extends StatelessWidget {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  // info column
-                  InfoWidget(),
-                  // Image with me
-                  SizedBox(
-                    width: 200,
-                    height: 200,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(50),
-                      child: Image.asset(
-                        'pozaCuMnLow.jpg',
+      body: Scrollable(
+          controller: _scrollController,
+          viewportBuilder: (context, position) {
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        // info column
+                        InfoWidget(),
+                        // Image with me
+                        SizedBox(
+                          width: 200,
+                          height: 200,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Image.asset(
+                              'pozaCuMnLow.jpg',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 35),
+                    const Center(
+                      child: FadingTextAnimation(
+                          text:
+                              "Anything is easy if you do what has to be done."),
+                    ),
+                    const SizedBox(height: 35),
+                    // about me section
+                    const Padding(
+                      padding: EdgeInsets.only(left: 50),
+                      child: Text(
+                        'About me',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 35),
-              const Center(
-                child: FadingTextAnimation(
-                    text: "Anything is easy if you do what has to be done."),
-              ),
-              const SizedBox(height: 35),
-              // about me section
-              const Padding(
-                padding: EdgeInsets.only(left: 50),
-                child: Text(
-                  'About me',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    const SizedBox(height: 15),
+                    AboutWidget(),
+                    const SizedBox(height: 35),
+                    // personal projects section
+                    const Padding(
+                      padding: EdgeInsets.only(left: 50),
+                      child: Text(
+                        'Personal projects',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 300,
+                      child: PersonalProjects(),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 15),
-              AboutWidget(),
-              const SizedBox(height: 35),
-              // personal projects section
-              const Padding(
-                padding: EdgeInsets.only(left: 50),
-                child: Text(
-                  'Personal projects',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                height: 300,
-                child: PersonalProjects(),
-              ),
-            ],
-          ),
-        ),
-      ),
+            );
+          }),
     );
   }
 }
