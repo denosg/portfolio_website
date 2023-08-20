eb0ba18b48fe95e2a254dcfc74d30ccf7b529793
denos <costelasdenis@gmail.com>
2023-08-20 17:19:38 +0300

    work exp model ready

---
+++ lib/models/work_exp.dart
@@ -0,0 +1,14 @@
+class WorkExp {
+  final String companyNameAndApp;
+  final String workTime;
+  final String linkForApp;
+  final List<String> tasksIveDone;
+  final String appImage;
+
+  WorkExp(
+      {required this.companyNameAndApp,
+      required this.workTime,
+      required this.linkForApp,
+      required this.tasksIveDone,
+      required this.appImage});
+}
