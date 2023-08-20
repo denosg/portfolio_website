a1988e8d725dfd02ca3b0b1005411c029d341b9d
denos <costelasdenis@gmail.com>
2023-08-20 19:33:20 +0300

    achievemnts are done

---
+++ lib/widgets/achievement_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AchievementWidget extends StatelessWidget {
+  final String title;
+  final String url;
+  final String photoUrl;
+  final bool isForDesk;
+  const AchievementWidget(
+      {super.key,
+      required this.title,
+      required this.url,
+      required this.photoUrl,
+      required this.isForDesk});
+
+  void openLink(String stringUrl) async {
+    final openUrl = Uri.parse(stringUrl);
+
+    if (await canLaunchUrl(openUrl)) {
+      await launchUrl(openUrl);
+    } else {
+      throw 'Could not launch $openUrl';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+
+    return Column(
+      children: [
+        // white space
+        SizedBox(height: deviceSize.height * 0.01),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Icon(Icons.arrow_right_rounded, color: Colors.purple),
+            Expanded(child: Text(title)),
+            IconButton(
+              icon: const Icon(Icons.link),
+              color: Colors.purple,
+              onPressed: () => openLink(url),
+            ),
+          ],
+        ),
+        SizedBox(height: deviceSize.height * 0.01),
+        Expanded(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.network(
+              photoUrl,
+              height:
+                  isForDesk ? deviceSize.height * 0.4 : deviceSize.height * 0.3,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/achievements_widget.dart
@@ -0,0 +1,123 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:portfolio_website/models/achievement.dart';
+import 'package:portfolio_website/providers/photo_list.dart';
+import 'package:portfolio_website/widgets/achievement_widget.dart';
+
+class AchievementsWidget extends StatefulWidget {
+  final bool isForDesk;
+  const AchievementsWidget({super.key, required this.isForDesk});
+
+  @override
+  State<AchievementsWidget> createState() => _AchievementsWidgetState();
+}
+
+class _AchievementsWidgetState extends State<AchievementsWidget> {
+  final controller = CarouselController();
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+    List<Achievement> achievements;
+
+    if (widget.isForDesk) {
+      achievements = achievementsDesk;
+    } else {
+      achievements = achievementsPhone;
+    }
+
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        if (achievements.length != 1)
+          Positioned(
+            left: deviceSize.width * 0.05,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                controller.previousPage();
+              },
+            ),
+          ),
+        Center(
+          child: SizedBox(
+            width: deviceSize.width * 0.8,
+            height: widget.isForDesk
+                ? deviceSize.height * 0.6
+                : deviceSize.height * 0.4,
+            child: CarouselSlider.builder(
+              carouselController: controller,
+              itemBuilder: (context, index, realIndex) {
+                final achievement = achievements[index];
+                return SizedBox(
+                  child: AchievementWidget(
+                    isForDesk: widget.isForDesk,
+                    title: achievement.title,
+                    photoUrl: achievement.photoUrl,
+                    url: achievement.url,
+                  ),
+                );
+              },
+              itemCount: achievements.length,
+              options: CarouselOptions(
+                height: widget.isForDesk
+                    ? deviceSize.height * 0.5
+                    : deviceSize.height * 0.4,
+                enlargeCenterPage: false,
+                viewportFraction: 1,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    currentPageIndex = index;
+                  });
+                },
+              ),
+            ),
+          ),
+        ),
+        if (achievements.length != 1)
+          Positioned(
+            right: deviceSize.width * 0.05,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_forward_ios),
+              onPressed: () {
+                controller.nextPage();
+              },
+            ),
+          ),
+      ],
+    );
+  }
+}
+
+List<Achievement> achievementsDesk = [
+  Achievement(
+    title: "1st Place County Phase TIC Olympiad",
+    url:
+        "https://drive.google.com/file/d/1_4_mLNrou-OkadUSMoVLP3WdX4GkMowy/view?usp=sharing",
+    photoUrl: ticOlympiad,
+  ),
+  Achievement(
+    title:
+        "1st Place in County Hackathon for Take Care App - An app connecting people needing services like medical assistance and shopping with specialized and non-specialized young people who can offer support. Advanced to semifinals in Bucharest.",
+    url:
+        "https://m.ebihoreanul.ro/stiri/trei-echipe-de-la-universitatea-din-oradea-remarcate-la-un-concurs-important-de-inovatie-ce-aplicatii-au-propus-177865.html?fbclid=IwAR22iI0b9CfiKA1mWRGf6szdgio1QjQEZL0jU3tCXa0fpZ3CNY_dITouQAw",
+    photoUrl: hackathonTakeCare,
+  ),
+];
+
+List<Achievement> achievementsPhone = [
+  Achievement(
+    title: "1st Place County Phase TIC Olympiad",
+    url:
+        "https://drive.google.com/file/d/1_4_mLNrou-OkadUSMoVLP3WdX4GkMowy/view?usp=sharing",
+    photoUrl: ticOlympiad,
+  ),
+  Achievement(
+    title:
+        "1st Place in County Hackathon for Take Care App. Advanced to semifinals in Bucharest.",
+    url:
+        "https://m.ebihoreanul.ro/stiri/trei-echipe-de-la-universitatea-din-oradea-remarcate-la-un-concurs-important-de-inovatie-ce-aplicatii-au-propus-177865.html?fbclid=IwAR22iI0b9CfiKA1mWRGf6szdgio1QjQEZL0jU3tCXa0fpZ3CNY_dITouQAw",
+    photoUrl: hackathonTakeCare,
+  ),
+];
