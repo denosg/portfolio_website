bd55d4ff4cfaaca9e54008c28c529aedc0522af5
denos <costelasdenis@gmail.com>
2023-09-04 21:39:46 +0300

    perf improvement for home page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -3,14 +3,9 @@ import './responsive/Widgets/desktopBody.dart';
 import './responsive/Widgets/mobileBody.dart';
 import './responsive/responsive_layout.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
