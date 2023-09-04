e99c3a62a25dfb29bfbd48643bfc555cc46fcb79
denos <costelasdenis@gmail.com>
2023-09-04 21:40:27 +0300

    hupu tasks

---
+++ lib/providers/text_hupu_app.dart
@@ -0,0 +1,13 @@
+List<String> tasksIveDoneHupu = [
+  'Worked on a healthcare mobile application for a hospital client. Made significant contributions to the front-end UI/UX design and functionality.',
+  'Developed custom UI screens and views for different user roles and use cases.',
+  'Implemented smooth navigation and intuitive workflows.',
+  'Built reusable component libraries improving code quality.',
+  'Implemented complex front-end business logic and validation.',
+  'Developed responsive design.',
+  'Contributed to testing and QA efforts to ensure robust functionality.',
+  'Collaborated with back-end and API teams on integration.',
+  'Participated in Scrum sprints, code reviews, and documentation.',
+  'Improved security to app.',
+  'Used version control practices to manage code changes and releases.'
+];
