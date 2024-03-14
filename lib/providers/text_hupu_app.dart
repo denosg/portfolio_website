84c2a89ddc621d1f4c04d2ec0ede74e69fa44e30
denos <costelasdenis@gmail.com>
2024-03-14 16:58:05 +0200

    misspelling

--- lib/providers/text_hupu_app.dart
+++ lib/providers/text_hupu_app.dart
@@ -1,7 +1,7 @@
 List<String> tasksIveDoneHupu = [
   'Played a pivotal role in developing a healthcare mobile application for a hospital client, focusing on front-end UI/UX design and functionality enhancements',
   'Engineered custom UI screens and views for diverse user roles and use cases, ensuring an intuitive user experience',
-  'mproved the decryption/encryption process, reducing data loss from 25% to 0%, enhancing data security',
+  'Improved the decryption/encryption process, reducing data loss from 25% to 0%, enhancing data security',
   'Implemented Google Cloud Functions and Scheduled Cloud Functions using Node.JS, enhancing backend functionality',
   'Established a development environment and separated development from production environments for operational stability',
   'Actively participated in Scrum sprints, conducted code reviews, and contributed to documentation efforts',
